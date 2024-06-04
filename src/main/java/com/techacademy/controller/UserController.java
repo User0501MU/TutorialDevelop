8c295b3d25ec2a0b7c3b42a0d7db8eebd29a6c93
User0501MU <m-utsunomiya@fores.jp>
2024-06-04 20:39:18 +0900

    Lesson 18Chapter 10

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,19 +1,21 @@
 //コントローラーの作成
 package com.techacademy.controller;
 
-import java.util.Set; // 追加User削除
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult; // 追加
+import org.springframework.validation.annotation.Validated; // 追加
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
-import org.springframework.web.bind.annotation.ModelAttribute; // 追加
-import org.springframework.web.bind.annotation.PostMapping; // 追加
-import com.techacademy.entity.User; // 追加
-
-import org.springframework.web.bind.annotation.PathVariable; // 追加
-import org.springframework.web.bind.annotation.RequestParam; // 追加User削除
-
 
 @Controller
 @RequestMapping("user")
@@ -33,7 +35,6 @@ public class UserController {
         return "user/list";
     }
 
- // ----- 追加:ここから -----
     /** User登録画面を表示 */
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
@@ -41,17 +42,21 @@ public class UserController {
         return "user/register";
     }
 
+    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- User登録機能：コントローラーの修正　追加:ここまで -----
+    // ----- 変更ここまで -----
 
-    // ----- 追加:ここから -----
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
@@ -69,10 +74,7 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // -----User変更機能 追加:ここまで -----
 
-
- // ----- 追加:ここから -----
     /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
     public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
@@ -81,6 +83,4 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // User削除----- 追加:ここまで -----
-
 }
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,4 +1,5 @@
 //エンティティの作成
+//入力チェック エンティティの修正
 package com.techacademy.entity;
 
 import jakarta.persistence.Column;
@@ -9,55 +10,51 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email; // 追加
+import jakarta.validation.constraints.Max; // 追加
+import jakarta.validation.constraints.Min; // 追加
+import jakarta.validation.constraints.NotEmpty; // 追加
+import jakarta.validation.constraints.NotNull; // 追加
+
+import org.hibernate.validator.constraints.Length; // 追加
+
 import lombok.Data;
 
 @Data
-//@Data はLombokのアノテーションです。「getter/setter、toString、hashCode、equals」のメソッドを生成します。
 @Entity
-//@Entity はSpring JPAのアノテーションです。エンティティクラス（データベースのテーブルにマッピングするクラス）であることを示します。
 @Table(name = "user")
-//@Table もSpring JPAのアノテーションです。エンティティが対応する（紐づく）テーブル名を指定します。今回はMySQLの user テーブルに対応しています。
-
 public class User {
 
     /** 性別用の列挙型 */
-	///** で始まるコメントは「Javadoc」というコメントの書き方 コメントのポップアップ表示や、ドキュメントの自動生成を行なうことができる
     public static enum Gender {
         男性, 女性
     }
 
     /** 主キー。自動生成 */
     @Id
-    //@Id はSpring JPAのアノテーションです。主キー
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@GeneratedValue はSpring JPAのアノテーションです。主キーの値を一意に自動生成
     private Integer id;
-
-    //GenerationType.AUTO：データベースに最適な方法が自動的に選択される。
-    //GenerationType.IDENTITY：自動インクリメントで生成する（データベースがOracleの場合はSEQUENCEと同じ処理を行なう）。
-    //GenerationType.SEQUENCE：シーケンスを使って生成する（データベースがMySQLの場合はTABLEと同じ処理を行なう）。
-    //GenerationType.TABLE：シーケンス専用テーブルを使って生成する。
 
     /** 名前。20桁。null不許可 */
     @Column(length = 20, nullable = false)
+    @NotEmpty // 追加
+    @Length(max=20) // 追加
     private String name;
 
     /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull // 追加
     private Gender gender;
 
     /** 年齢 */
+    @Min(0) // 追加
+    @Max(120) // 追加
     private Integer age;
 
     /** メールアドレス。50桁。null許可 */
     @Column(length = 50)
+    @Email // 追加
+    @Length(max=50) // 追加
     private String email;
-
-    //@Column はSpring JPAのアノテーションです。フィールドをマッピングするテーブルのカラム（項目）を指定します。今回は使用していませんが、name="カラム名"でマッピングするカラム名を指定できます。name属性を省略した場合、フィールド名がマッピングするカラム名となります。属性の意味は以下のとおりです。
-    //length : フィールドの桁数を指定します。桁数指定ができるフィールドは文字列型か配列型のみです。数値型は指定できません。
-    //nullable フィールドのNOT NULL制約を指定します。値が「true」のときはnull値を許可し、「false」のときはnull値を許可しません。デフォルト値は「true」です。
-    //@Enumerated はSpring JPAのアノテーションです。このフィールドの型が列挙型であることを指定します。 EnumType.STRING 属性でテーブルのカラムを文字列型に指定します。これにより、カラムには「男性」などの列挙子の名前が格納されます。
-
-
 }
