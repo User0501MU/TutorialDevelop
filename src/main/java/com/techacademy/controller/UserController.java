cd6b56c7d62d42ae53728dae805b26640bad3815
User0501MU <m-utsunomiya@fores.jp>
2024-06-04 15:28:35 +0900

    Lesson 18Chapter9 User登録・更新・削除

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,11 +1,19 @@
 //コントローラーの作成
 package com.techacademy.controller;
 
+import java.util.Set; // 追加User削除
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.techacademy.service.UserService;
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
+import com.techacademy.entity.User; // 追加
+
+import org.springframework.web.bind.annotation.PathVariable; // 追加
+import org.springframework.web.bind.annotation.RequestParam; // 追加User削除
+
 
 @Controller
 @RequestMapping("user")
@@ -24,4 +32,55 @@ public class UserController {
         // user/list.htmlに画面遷移
         return "user/list";
     }
+
+ // ----- 追加:ここから -----
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+
+    /** User登録処理 */
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- User登録機能：コントローラーの修正　追加:ここまで -----
+
+    // ----- 追加:ここから -----
+    /** User更新画面を表示 */
+    @GetMapping("/update/{id}/")
+    public String getUser(@PathVariable("id") Integer id, Model model) {
+        // Modelに登録
+        model.addAttribute("user", service.getUser(id));
+        // User更新画面に遷移
+        return "user/update";
+    }
+
+    /** User更新処理 */
+    @PostMapping("/update/{id}/")
+    public String postUser(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // -----User変更機能 追加:ここまで -----
+
+
+ // ----- 追加:ここから -----
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // User削除----- 追加:ここまで -----
+
 }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -2,21 +2,48 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set; // 追加User削除
 import org.springframework.stereotype.Service;
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public UserService(UserRepository repository) {
-        this.userRepository = repository;
-    }
+	public UserService(UserRepository repository) {
+		this.userRepository = repository;
+	}
 
-    /** 全件を検索して返す */
-    public List<User> getUserList() {
-        // リポジトリのfindAllメソッドを呼び出す
-        return userRepository.findAll();
-    }
+	/** 全件を検索して返す */
+	public List<User> getUserList() {
+		// リポジトリのfindAllメソッドを呼び出す
+		return userRepository.findAll();
+	}
+
+	// ----- 追加:ここから -----
+	/** Userを1件検索して返す */
+	public User getUser(Integer id) {
+		return userRepository.findById(id).get();
+	}
+	// -----User変更機能 追加:ここまで -----
+
+	// ----- 追加:ここから -----
+	/** Userの登録を行なう */
+	@Transactional
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+	// ----- User登録機能：サービスの修正追加:ここまで -----
+
+	// ----- 追加:ここから -----
+	/** Userの削除を行なう */
+	@Transactional
+	public void deleteUser(Set<Integer> idck) {
+		for (Integer id : idck) {
+			userRepository.deleteById(id);
+		}
+	}
+	// ----- Userの削除追加:ここまで -----
 }
